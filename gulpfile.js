960d3a482a128e62aba232066b77f18f7b84e49f
Sylwester K. Mielniczuk <sylwester@mielniczuk.com>
2017-10-21 19:55:02 +0100

    Updated index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
 var browserSync = require('browser-sync').create();
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('hello', function() {
   console.log('Hello Zell');
@@ -15,7 +35,6 @@ gulp.task('sass', function() {
     }))
 });
 
-
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
@@ -24,7 +43,7 @@ gulp.task('browserSync', function() {
   })
 })
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
+gulp.task('watch', ['browserSync', 'sass' , 'useref', 'images'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload); 
   gulp.watch('app/js/**/*.js', browserSync.reload); 	
